f7284ddbfc24df0f7f8b063fccb1ae2c9ed764d2
chomado <chomado@gmail.com>
2016-12-03 16:48:11 +0900

    取得データ元を変更

--- DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
+++ DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
@@ -58,19 +58,12 @@ namespace DevDaysSpeakers.ViewModel
 			{
 				IsBusy = true;
 
-				using (var client = new HttpClient())
-				{
-					//サーバーから json を取得します
-					var json = await client.GetStringAsync("http://demo4404797.mockable.io/speakers");
+				var service = DependencyService.Get<AzureService>();
+				var items = await service.GetSpeakers();
 
-					//json をデシリアライズします
-					var items = JsonConvert.DeserializeObject<List<Speaker>>(json);
-
-					//リストを Speakers に読み込ませます
-					Speakers.Clear();
-					foreach (var item in items)
-						Speakers.Add(item);
-				}
+				Speakers.Clear();
+				foreach (var item in items)
+					Speakers.Add(item);
 			}
 			catch (Exception ex)
 			{
