78af3c0a270109f2676725ac3d145cae16a3f5f9
chomado <chomado@gmail.com>
2016-12-03 15:15:45 +0900

    Create GetSpeakers Command

--- DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
+++ DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
@@ -19,9 +19,14 @@ namespace DevDaysSpeakers.ViewModel
 	public class SpeakersViewModel : INotifyPropertyChanged
 	{
 		public ObservableCollection<Speaker> Speakers { get; set; }
+		public Command GetSpeakersCommand { get; set; }
+
 		public SpeakersViewModel() 
 		{
 			Speakers = new ObservableCollection<Speaker>();
+			GetSpeakersCommand = new Command(
+				async () => await GetSpeakers(),
+				() => !IsBusy);
 		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
@@ -33,11 +38,52 @@ namespace DevDaysSpeakers.ViewModel
 		public bool IsBusy
 		{ 
 			get { return busy; }
-			set {
+			set
+			{
 				busy = value;
 				OnPropertyChanged();
+				//Update the can execute
+				GetSpeakersCommand.ChangeCanExecute();
 			}
 		}
 
+		// インターネットから speaker のデータをすべて取ってくる
+		async Task GetSpeakers()
+		{
+			if (IsBusy)
+				return;
+
+			Exception error = null;
+			try
+			{
+				IsBusy = true;
+
+				using (var client = new HttpClient())
+				{
+					//サーバーから json を取得します
+					var json = await client.GetStringAsync("http://demo4404797.mockable.io/speakers");
+
+					//json をデシリアライズします
+					var items = JsonConvert.DeserializeObject<List<Speaker>>(json);
+
+					//リストを Speakers に読み込ませます
+					Speakers.Clear();
+					foreach (var item in items)
+						Speakers.Add(item);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Error: " + ex);
+				error = ex;
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+
+			if (error != null)
+				await Application.Current.MainPage.DisplayAlert("Error!", error.Message, "OK");
+		}
 	}
 }
